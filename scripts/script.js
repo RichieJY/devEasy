20c10d8ab4cdb09846539988c271187431162ad1
Richie Y <ryoder@una.edu>
2025-03-11 12:10:46 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    console.log("Page Loaded/Ready"); 
+
+
+const facts = [];
+facts[0] = "Richie's favorite color is purple";
+facts[1] = "Reed has two brothers";
+facts[2] = "BeastCarp1 likes to swim";
+facts[3] = "Jaxon has broken a finger before";
+facts[4] = "Victor is ambidextrous";
+facts[5] = "Olivia can say her alphabet backwards";
+facts[6] = "Corey is not famous";
+facts[7] = "Tatum was born in Lousiana";
+facts[8] = "Kaden has never tried a PB&J Sandwhich";
+facts[9] = "Dylan is allergic to cats";
+facts[8] = "SSenecalI01 likes trains";
+facts[9] = "Daniel knows the secret identity of beastcarp1";
+facts[10] = "Matthew has a dog";
+facts[11] = "Jacob hates the parking at UNA";
+facts[12] = "mandrews3 has three dogs";
+facts[13] = "Carly is adopted from China";
+facts[14] = "ccantrell2 used to have a Hummer H3";
+facts[15] = "carsonkerr plays on the rugby team";
+facts[16] = "Gage thinks UNA Basketball 'sold the bag'";
+facts[17] = "Corbin advises the Great Wall of China is 13,000 miles long";
+facts[18] = "hnguyen1995 hates jS";
+facts[19] = "llemoine1230 loves Romans 5:8";
+facts[20] = "ehb2004 loves to run";
+
+
+document.getElementById("demo").innerHTML = facts.toString();
+
+
+
+});
